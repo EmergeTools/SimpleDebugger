b00716c5ea539249bfe3f5d933e5982c88a8f012
Noah Martin <noahm444@gmail.com>
2025-01-29 18:23:18 -0500

    Fixes for intel

--- Sources/SimpleDebugger/emg_vm_protect.c
+++ Sources/SimpleDebugger/emg_vm_protect.c
@@ -8,6 +8,8 @@
 #import "emg_vm_protect.h"
 #import <mach/mach.h>
 
+#if defined(__arm64__) || defined(__aarch64__)
+
 extern kern_return_t _kern_rpc_emg_vm_prot_trap(
   mach_port_name_t target,
   mach_vm_address_t address,
@@ -29,3 +31,5 @@ asm(
 kern_return_t emg_vm_protect(mach_port_t target, mach_vm_address_t address, mach_vm_size_t size, boolean_t set_maximum, vm_prot_t new_protection) {
   return _kern_rpc_emg_vm_prot_trap(target, address, size, set_maximum, new_protection);
 }
+
+#endif
